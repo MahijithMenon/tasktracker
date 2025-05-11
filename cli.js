0bdbeeed0c1b4f7b1648027c4423b11d1b703066
Mahijith K Menon <menon.mahijith@gmail.com>
2025-05-11 18:29:01 +0530

    url changed

--- cli.js
+++ cli.js
@@ -2,14 +2,15 @@
 const axios = require("axios");
 const { text } = require("express");
 const args = process.argv.slice(2);
+const url = process.env.URL
 
 if (args[0] === "add" && args[1]) {
-    axios.post("http://localhost:3000/tasks", { text: args.slice(1).join(" ") })
+    axios.post(`${url}/tasks`, { text: args.slice(1).join(" ") })
         .then(response => console.log(response.data.message, `(ID: ${response.data.task.id})`))
         .catch(error => console.error("Error adding task:", error.response?.data || error.message));
 } 
 else if (args[0] === "delete" && args[1]){
-    axios.delete("http://localhost:3000/tasks",{
+    axios.delete(`${url}/tasks`,{
         data: { id: Number(args[1]) }
     })
     .then(response => console.log(response.data.message, `(ID: ${response.data.id})`))
@@ -17,21 +18,21 @@ else if (args[0] === "delete" && args[1]){
 
 }
 else if (args[0] === "update" && args[1] || args[2] || args[3]){
-    axios.put("http://localhost:3000/tasks",{
+    axios.put(`${url}/tasks`,{
      id: Number(args[1]),text:args.slice(2,3).join(""),description:args.slice(3).join(""),
     })
     .then(response => console.log(response.data.message, `(ID: ${response.data.id})`))
     .catch(error => console.error("Error Updating task:", error.response?.data || error.message));
 }
 else if (args[0] === "mark-in-progress" && args[1]){
-    axios.put("http://localhost:3000/status",{
+    axios.put(`${url}/status`,{
      id: Number(args[1]),status:'in-progress'
     })
     .then(response => console.log(response.data.message, `(ID: ${response.data.id})`))
     .catch(error => console.error("Error Updating task:", error.response?.data || error.message));
 }
 else if (args[0] === "mark-done" && args[1]){
-    axios.put("http://localhost:3000/status",{
+    axios.put(`${url}/status`,{
      id: Number(args[1]),status:'done'
     })
     .then(response => console.log(response.data.message, `(ID: ${response.data.id})`))
@@ -39,12 +40,12 @@ else if (args[0] === "mark-done" && args[1]){
 }
 else if(args[0]==="list"){
 if(args[1]==="done" || args[1]==="todo" || args[1]==="in-progress"){
-    axios.get(`http://localhost:3000/list?filter=${args[1]}`)
+    axios.get(`${url}/list?filter=${args[1]}`)
     .then(response => console.log(response.data.message, response.data))
     .catch(error => console.error("Error Updating task:", error.response?.data || error.message));
     return;
 }
-axios.get(`http://localhost:3000/list?filter=all}`)
+axios.get(`${url}/list?filter=all}`)
     .then(response => console.log(response.data.message, response.data))
     .catch(error => console.error("Error Updating task:", error.response?.data || error.message));
 
